d3891617c44457e04db0bba2126902084507ad8f
Piotr Migdał <pmigdal@gmail.com>
2019-10-31 13:00:47 +0800

    chore(eslint) removed prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,13 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "prettier"],
+  plugins: ["@typescript-eslint"],
   extends: [
     "plugin:@typescript-eslint/recommended",
-    // "plugin:prettier/recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript"
   ],
   rules: {
-    // "prettier/prettier": "error",
     // "no-this-alias": false,
     "@typescript-eslint/no-this-alias": "off",
     '@typescript-eslint/member-delimiter-style': ['off', { // Prevents us from using any delimiter for interface properties.
