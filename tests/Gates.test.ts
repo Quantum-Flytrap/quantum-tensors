f5d20f0b240328b97e7ce8161040abfe95e630c3
Piotr Migdal <pmigdal@gmail.com>
2020-02-09 16:12:13 +0100

    test(Gates) testing Pauli gates

--- tests/Gates.test.ts
+++ tests/Gates.test.ts
@@ -1,7 +1,49 @@
+import { Cx } from '../src/Complex'
 import * as Gates from '../src/Gates'
 
-describe('X Gate', () => {
-  it('should create 0<->1', () => {
-    expect(Gates.X().toString('cartesian', 2, ' + ', false)).toEqual('(1.00 +0.00i) |0⟩⟨1| + (1.00 +0.00i) |1⟩⟨0|')
+describe('X, Y, Z Gate', () => {
+  const X = Gates.X()
+  const Y = Gates.Y()
+  const Z = Gates.Z()
+
+  it('X should create 0<->1', () => {
+    expect(X.toDense()).toEqual([
+      [Cx(0), Cx(1)],
+      [Cx(1), Cx(0)],
+    ])
+  })
+
+  it('Y', () => {
+    expect(Y.toDense()).toEqual([
+      [Cx(0), Cx(0, -1)],
+      [Cx(0, 1), Cx(0)],
+    ])
+  })
+
+  it('Z', () => {
+    expect(Z.toDense()).toEqual([
+      [Cx(1), Cx(0)],
+      [Cx(0), Cx(-1)],
+    ])
+  })
+
+  it('X^2 should be identity', () => {
+    expect(X.mulOp(X).toDense()).toEqual(Gates.I().toDense())
+  })
+
+  it('X * Y = i Z', () => {
+    const lhs = X.mulOp(Y).toDense()
+    const rhs = Z.mulConstant(Cx(0, 1)).toDense()
+    // see https://www.npmjs.com/package/jest-matcher-deep-close-to (no types!)
+    // https://github.com/facebook/jest/issues/4058
+    // or do something with toDense for the sake of tests
+    expect(lhs[0][0].re).toBeCloseTo(rhs[0][0].re)
+    expect(lhs[0][0].im).toBeCloseTo(rhs[0][0].im)
+    expect(lhs[1][0].re).toBeCloseTo(rhs[1][0].re)
+    expect(lhs[1][0].im).toBeCloseTo(rhs[1][0].im)
+    expect(lhs[0][1].re).toBeCloseTo(rhs[0][1].re)
+    expect(lhs[0][1].im).toBeCloseTo(rhs[0][1].im)
+    expect(lhs[1][1].re).toBeCloseTo(rhs[1][1].re)
+    expect(lhs[1][1].im).toBeCloseTo(rhs[1][1].im)
   })
 })
