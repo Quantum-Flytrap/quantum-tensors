4335d9c8e926a13a7fe95cef739b9d2709e13b0e
Piotr Migdal <pmigdal@gmail.com>
2020-02-14 03:11:29 +0100

    test(Gates) checks is all are unitary

--- tests/Gates.test.ts
+++ tests/Gates.test.ts
@@ -8,33 +8,62 @@ describe('X, Y, Z Gate', () => {
   const Z = Gates.Z()
 
   it('X should create 0<->1', () => {
+    expect(X.isCloseToUnitary()).toBe(true)
     expect(X).operatorCloseToNumbers([
       [Cx(0), Cx(1)],
       [Cx(1), Cx(0)],
     ])
+    expect(X.mulOp(X).isCloseToIdentity()).toBe(true)
   })
 
   it('Y', () => {
+    expect(Y.isCloseToUnitary()).toBe(true)
     expect(Y).operatorCloseToNumbers([
       [Cx(0), Cx(0, -1)],
       [Cx(0, 1), Cx(0)],
     ])
+    expect(Y.mulOp(Y).isCloseToIdentity()).toBe(true)
   })
 
   it('Z', () => {
+    expect(Z.isCloseToUnitary()).toBe(true)
     expect(Z).operatorCloseToNumbers([
       [Cx(1), Cx(0)],
       [Cx(0), Cx(-1)],
     ])
-  })
-
-  it('X^2 should be identity', () => {
-    expect(X.mulOp(X)).operatorCloseToNumbers(Gates.I().toDense())
+    expect(Z.mulOp(Z).isCloseToIdentity()).toBe(true)
   })
 
   it('X * Y = i Z', () => {
     const lhs = X.mulOp(Y)
     const rhs = Z.mulConstant(Cx(0, 1))
     expect(lhs).operatorCloseToNumbers(rhs.toDense())
+  })
+})
+
+describe('All gates', () => {
+  it('identity to be identity', () => {
+    expect(Gates.I().isCloseToIdentity()).toBe(true)
+  })
+
+  it('all 1-qubit gates are unitary', () => {
+    expect(Gates.I().isCloseToUnitary()).toBe(true)
+    expect(Gates.X().isCloseToUnitary()).toBe(true)
+    expect(Gates.Y().isCloseToUnitary()).toBe(true)
+    expect(Gates.Z().isCloseToUnitary()).toBe(true)
+    expect(Gates.H().isCloseToUnitary()).toBe(true)
+    expect(Gates.S().isCloseToUnitary()).toBe(true)
+    expect(Gates.T().isCloseToUnitary()).toBe(true)
+  })
+
+  it('all 2-qubit gates are unitary', () => {
+    expect(Gates.CX().isCloseToUnitary()).toBe(true)
+    expect(Gates.CZ().isCloseToUnitary()).toBe(true)
+    expect(Gates.Swap().isCloseToUnitary()).toBe(true)
+  })
+
+  it('all 3qubit gates are unitary', () => {
+    expect(Gates.CCX().isCloseToUnitary()).toBe(true)
+    expect(Gates.CSwap().isCloseToUnitary()).toBe(true)
   })
 })
