d50205fe058cf802aabf9e9b42febb04a1740690
Piotr Migdal <pmigdal@gmail.com>
2020-03-02 21:06:39 +0100

    feat(Elements) incoherent light

--- tests/Elements.test.ts
+++ tests/Elements.test.ts
@@ -1,3 +1,4 @@
+import { Cx } from '../src/Complex'
 import * as Elements from '../src/Elements'
 import './customMatchers'
 
@@ -5,21 +6,45 @@ describe('Optical elements', () => {
   it('Sugar solution', () => {
     expect(Elements.sugarSolution().isCloseToUnitary()).toBe(true)
     expect(Elements.sugarSolution().isCloseToHermitian()).toBe(false)
+    expect(Elements.incoherentLightOperator(Elements.sugarSolution())).operatorCloseToNumbers([
+      [Cx(1), Cx(0), Cx(0), Cx(0)],
+      [Cx(0), Cx(1), Cx(0), Cx(0)],
+      [Cx(0), Cx(0), Cx(1), Cx(0)],
+      [Cx(0), Cx(0), Cx(0), Cx(1)],
+    ])
   })
 
   it('Attenuator', () => {
     expect(Elements.attenuator().isCloseToUnitary()).toBe(false)
     expect(Elements.attenuator().isCloseToHermitian()).toBe(true)
+    expect(Elements.incoherentLightOperator(Elements.attenuator())).operatorCloseToNumbers([
+      [Cx(0.5), Cx(0), Cx(0), Cx(0)],
+      [Cx(0), Cx(0.5), Cx(0), Cx(0)],
+      [Cx(0), Cx(0), Cx(0.5), Cx(0)],
+      [Cx(0), Cx(0), Cx(0), Cx(0.5)],
+    ])
   })
 
   it('Vacuum Jar (phase advancer)', () => {
     expect(Elements.vacuumJar().isCloseToUnitary()).toBe(true)
     expect(Elements.vacuumJar().isCloseToHermitian()).toBe(false)
+    expect(Elements.incoherentLightOperator(Elements.vacuumJar())).operatorCloseToNumbers([
+      [Cx(1), Cx(0), Cx(0), Cx(0)],
+      [Cx(0), Cx(1), Cx(0), Cx(0)],
+      [Cx(0), Cx(0), Cx(1), Cx(0)],
+      [Cx(0), Cx(0), Cx(0), Cx(1)],
+    ])
   })
 
   it('Glass Slab (phase retarder)', () => {
     expect(Elements.glassSlab().isCloseToUnitary()).toBe(true)
     expect(Elements.glassSlab().isCloseToHermitian()).toBe(false)
+    expect(Elements.incoherentLightOperator(Elements.glassSlab())).operatorCloseToNumbers([
+      [Cx(1), Cx(0), Cx(0), Cx(0)],
+      [Cx(0), Cx(1), Cx(0), Cx(0)],
+      [Cx(0), Cx(0), Cx(1), Cx(0)],
+      [Cx(0), Cx(0), Cx(0), Cx(1)],
+    ])
   })
 
   it('Mirror', () => {
@@ -57,14 +82,32 @@ describe('Optical elements', () => {
   it('Corner cube', () => {
     expect(Elements.cornerCube().isCloseToUnitary()).toBe(true)
     expect(Elements.cornerCube().isCloseToHermitian()).toBe(true)
+    expect(Elements.incoherentLightOperator(Elements.cornerCube())).operatorCloseToNumbers([
+      [Cx(0), Cx(0), Cx(1), Cx(0)],
+      [Cx(0), Cx(0), Cx(0), Cx(1)],
+      [Cx(1), Cx(0), Cx(0), Cx(0)],
+      [Cx(0), Cx(1), Cx(0), Cx(0)],
+    ])
   })
 
   it('Polarizing beam splitter', () => {
     expect(Elements.polarizingBeamsplitter(0).isCloseToUnitary()).toBe(true)
     expect(Elements.polarizingBeamsplitter(0).isCloseToHermitian()).toBe(true)
+    expect(Elements.incoherentLightOperator(Elements.polarizingBeamsplitter(0))).operatorCloseToNumbers([
+      [Cx(0.5), Cx(0.5), Cx(0), Cx(0)],
+      [Cx(0.5), Cx(0.5), Cx(0), Cx(0)],
+      [Cx(0), Cx(0), Cx(0.5), Cx(0.5)],
+      [Cx(0), Cx(0), Cx(0.5), Cx(0.5)],
+    ])
 
     expect(Elements.polarizingBeamsplitter(90).isCloseToUnitary()).toBe(true)
     expect(Elements.polarizingBeamsplitter(90).isCloseToHermitian()).toBe(true)
+    expect(Elements.incoherentLightOperator(Elements.polarizingBeamsplitter(90))).operatorCloseToNumbers([
+      [Cx(0.5), Cx(0), Cx(0), Cx(0.5)],
+      [Cx(0), Cx(0.5), Cx(0.5), Cx(0)],
+      [Cx(0), Cx(0.5), Cx(0.5), Cx(0)],
+      [Cx(0.5), Cx(0), Cx(0), Cx(0.5)],
+    ])
   })
 
   it('Faraday rotator', () => {
