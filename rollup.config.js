8257affc769604ee8660f15f560418d9ffacd937
Piotr Migdal <pmigdal@gmail.com>
2020-03-10 11:08:36 +0100

    feat(build) rollup for browser builds init

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
+import { terser } from 'rollup-plugin-terser'
+
+// delete old typings to avoid issues
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+require('fs').unlink('dist/src/index.d.ts', () => {})
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      name: 'QuantumTensors',
+      compact: true,
+      exports: 'named',
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+      name: 'QuantumTensors',
+      exports: 'named',
+    },
+    {
+      file: pkg.browser,
+      format: 'iife',
+      name: 'QuantumTensors',
+      compact: true,
+      exports: 'named',
+    },
+  ],
+  external: [...Object.keys(pkg.dependencies || {})],
+  plugins: [
+    typescript({
+      typescript: require('typescript'),
+    }),
+    terser(),
+  ],
+}
