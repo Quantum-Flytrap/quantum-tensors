811404b9ddc18a679cdc642c6272f774a412593f
Philippe Cochin <philippe.cochin@gmail.com>
2020-08-07 18:40:02 +0200

    Gauss complex multiplication algorithm

--- src/Complex.ts
+++ src/Complex.ts
@@ -106,9 +106,23 @@ export default class Complex {
    * @param z2 complex number to be multiplied
    * @returns z = z1 * z2
    */
-  mul(z2: Complex): Complex {
+  mulNaive(z2: Complex): Complex {
     const z1 = this
     return new Complex(z1.re * z2.re - z1.im * z2.im, z1.re * z2.im + z1.im * z2.re)
+  }
+
+  /**
+   * Gauss complex multiplication algorithm
+   * https://en.wikipedia.org/wiki/Multiplication_algorithm#Complex_multiplication_algorithm
+   * @param z2 complex number to be multiplied
+   * @returns z = z1 * z2
+   */
+  mul(z2: Complex): Complex {
+    const z1 = this
+    const k1 = z2.re * (z1.re + z1.im)
+    const k2 = z1.re * (z2.im - z2.re)
+    const k3 = z1.im * (z2.re + z2.im)
+    return new Complex(k1 - k3, k1 + k2)
   }
 
   /**
