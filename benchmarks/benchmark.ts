7331d248d17553915d78e887252106aeb1e15a3f
Philippe Cochin <philippe.cochin@gmail.com>
2020-09-16 16:59:55 +0200

    Switched to benny

--- benchmarks/benchmark.ts
+++ benchmarks/benchmark.ts
@@ -1,17 +1,22 @@
+// See infos at https://nodejs.org/api/perf_hooks.html#perf_hooks_performance_now
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { performance } = require('perf_hooks')
 import Simulation from '../src/Simulation'
 import { grid1, grid2 } from './levels'
 
 console.log('---Running benchmarks---')
-console.time('100 frames')
 let sim = new Simulation(grid1)
 let laserIndicator = sim.generateLaserIndicator()
 sim.initializeFromIndicator(laserIndicator)
+const t0 = performance.now()
 sim.generateFrames(100)
-console.timeEnd('100 frames')
+const t1 = performance.now()
+console.log(`Grid 1 (100 frames) took ${t1 - t0} milliseconds.`)
 
-console.time('Hall of mirrors')
 sim = new Simulation(grid2)
 laserIndicator = sim.generateLaserIndicator()
 sim.initializeFromIndicator(laserIndicator)
+const t2 = performance.now()
 sim.generateFrames(100)
-console.timeEnd('Hall of mirrors')
+const t3 = performance.now()
+console.log(`Grid 2 (100 frames) took ${t3 - t2} milliseconds.`)
--- benchmarks/complexMul.ts
+++ benchmarks/complexMul.ts
@@ -1,23 +1,29 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const Benchmark = require('benchmark')
+const b = require('benny')
 import Complex from '../src/Complex'
-const suite = new Benchmark.Suite()
 
-const z1 = new Complex(0.58, 1.68)
-const z2 = new Complex(-1.6, 0.94)
+/**
+ * Random float generator
+ */
+function randFloat(min = -10000, max = 10000): number {
+  return (Math.random() * (max - min) + min) / 100
+}
+const z1 = new Complex(randFloat(), randFloat())
+const z2 = new Complex(randFloat(), randFloat())
 
 // add tests
-suite
-  .add('ComplexMultiply#Naive', function () {
+b.suite(
+  'ComplexMul algorithms',
+
+  b.add('ComplexMultiply#Naive', function () {
     z1.mul(z2)
-  })
-  .add('ComplexMultiply#Gauss', function () {
+  }),
+
+  b.add('ComplexMultiply#Gauss', function () {
     z1.mulGauss(z2)
-  })
-  // add listeners
-  .on('cycle', function (event: Event) {
-    console.log(String(event.target))
-  })
-  .on('complete', () => console.log('Fastest is ' + suite.filter('fastest').map('name')))
-  // run async
-  .run({ async: true })
+  }),
+
+  b.cycle(),
+
+  b.complete(),
+)
